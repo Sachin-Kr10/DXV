156363a5af416a51a2e80840cc43e8db55a20231
Sachin Kumar <Sachinjnv100@outlook.com>
2026-02-02 02:41:21 +0530

    changes .env

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { authStore } from "./authStore";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true
 });
 
