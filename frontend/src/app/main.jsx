e2f6eef260401fd576a9eb07c4c165fbb245b0a5
Sachin Kumar <Sachinjnv100@outlook.com>
2026-02-02 03:54:42 +0530

    file namecase change

--- frontend/src/app/main.jsx
+++ frontend/src/app/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import {BrowserRouter} from 'react-router'
 import '../styles/main.css'
-import App from './app.jsx'
+import App from "./app.jsx"
 import { CartProvider } from '../context/cartcontext.jsx'
 import { AuthProvider } from "../context/authcontext.jsx";
 
