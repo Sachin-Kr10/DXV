3bfdc4a5e40f04ec3d5facd473412cabe3d4fad7
Sachin Kumar <Sachinjnv100@outlook.com>
2026-02-02 16:48:45 +0530

    use smtp.mail

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,12 +1,16 @@
 const transporter = require("../config/mail");
 
 const sendEmail = async ({ to, subject, text }) => {
-  await transporter.sendMail({
-    from: process.env.EMAIL_USER,
-    to,
-    subject,
-    text
-  });
+  try {
+    await transporter.sendMail({
+      from: `"DXV Support" <${process.env.EMAIL_USER}>`,
+      to,
+      subject,
+      text
+    });
+  } catch (error) {
+    throw error;
+  }
 };
 
 module.exports = sendEmail;
